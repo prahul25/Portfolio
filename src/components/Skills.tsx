a4422eb73b53091480519d1a4fde61efad1764e9
prahul25 <palrahulkumar2002@gmail.com>
2024-08-16 17:23:13 +0530

    Text cursor icon removed

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -30,7 +30,7 @@ function Skills() {
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="h-full flex flex-col lg:flex-row overflow-hidden items-center justify-center bg-black w-full gap-4 mx-auto px-8 relative"
+      className="h-full flex flex-col select-none lg:flex-row overflow-hidden items-center justify-center bg-black w-full gap-4 mx-auto px-8 relative"
     >
       <div className='relative z-20 w-full flex flex-col justify-center items-center mt-16'>
     <div className={`px-10 py-4 flex justify-center items-center text-4xl border-[3px] rounded-xl dark:hover:text-[#111827] dark:hover:bg-red-400 dark:hover:border-[#111827] hover:text-[#f0f0ff] hover:bg-[#6666FF] hover:border-[#f0f0ff] text-[#6666FF] border-[#6666FF] dark:text-red-400 dark:border-red-400 dark:bg-[#111827] bg-[#f0f0ff] ${frukturFont.className}`}>
