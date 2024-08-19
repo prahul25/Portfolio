b417516e823eaa855ecc2998be0078257f96d02f
prahul25 <palrahulkumar2002@gmail.com>
2024-08-19 23:38:24 +0530

    Added Contact component and include in page section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Contact from "@/components/Contact";
 import LandingPage from "@/components/LandingPage";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
@@ -9,6 +10,7 @@ export default function Home() {
     <LandingPage/>
     <Skills/>
     <Projects/>
+    <Contact/>
     </div>
   );
 }
